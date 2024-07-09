e0b2615471317f949e6da831313ee1e08b910b0e
Volodymyr Nerovnia <nerv@i.ua>
2024-07-10 02:55:16 +0300

    Enhanced index.js

--- index.js
+++ index.js
@@ -2,8 +2,13 @@
 
 import { processArrayTasks } from './src/tasks.js';
 
-const result = processArrayTasks([6,2,4,3,7,1]);
-//const result = processArrayTasks([4,8,5,6,1,6,5,8,4]);
-console.log(result.reverseArray);
-console.log(result.sumEven);
-console.log(result.isPalindrome);
+const testArrs = [
+  [6,2,4,3,7,1],
+  [4,8,5,6,1,6,5,8,4]
+];
+
+testArrs.map((arr) => {
+  const result = processArrayTasks(arr);
+  console.table(result);
+})
+
